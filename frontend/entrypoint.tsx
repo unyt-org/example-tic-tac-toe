84fcb3f1e570873764d926d36f19b86155c52e80
Jonas Strehle <jonas@strehles.info>
2023-09-17 11:40:07 +0200

    Add temp edpoint behaviour

--- frontend/entrypoint.tsx
+++ frontend/entrypoint.tsx
@@ -1,9 +1,17 @@
-import { UIX } from "uix/uix.ts";
+import { type UIX } from "uix/uix.ts";
 import { Game } from "backend/Map.ts";
 import { GamePage } from "common/components/GamePage.tsx";
+import { Datex } from "unyt_core/datex.ts";
 
 export default {
-	'/': <GamePage game={await Game.get(Math.random().toString(36).slice(2, 6))}/>,
-	'/:id': async (ctx) =>
-		<GamePage game={await Game.get(ctx.urlMatch.get("id")!)}/>,
+	'/': async () => {
+		const id = Math.random().toString(36).slice(2, 6);
+		await Datex.Supranet.connectAnonymous();
+		return <GamePage game={await Game.get(id)}/>;
+	},
+	'/:id': async (ctx) => {
+		const id = ctx.urlMatch.get("id")!;
+		await Datex.Supranet.connectAnonymous();
+		return <GamePage game={await Game.get(id)}/>;
+	}
 } satisfies UIX.Entrypoint;
