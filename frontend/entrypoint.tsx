b7250857c450555ffc11d3f18f33a317c68d68fb
Jonas Strehle <jonas@strehles.info>
2023-11-07 22:40:27 +0100

    Fix mode

--- frontend/entrypoint.tsx
+++ frontend/entrypoint.tsx
@@ -3,6 +3,11 @@ import type { Entrypoint } from "uix/html/entrypoints.ts"
 import { Game } from "../backend/Map.ts";
 import { GamePage } from "common/components/GamePage.tsx";
 import { Datex } from "unyt_core/datex.ts";
+import { UIX } from "uix";
+
+UIX.Theme.useThemes("uix-dark-plain");
+UIX.Theme.setMode("dark");
+
 // TODO: import backend/Map.ts does not load backend exports (must be relative with ../)?
 await Datex.Supranet.connectAnonymous();
 export default {
